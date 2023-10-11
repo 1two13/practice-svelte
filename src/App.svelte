66d4e41e2943aad3399d8d14bf77f8ccc3eb842d
Tory <88531407+1two13@users.noreply.github.com>
2023-10-11 16:45:51 +0900

    {#each ...}
    반복문

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,16 @@
 <script>
-  let auth = {
-    loggedIn: false
-  }
-
-  const handleLogin = () => auth.loggedIn = true;
-  const handleLogout = () => auth.loggedOut = false;
+  let todos = [
+    {id: 0, content: '1번', done: false},
+    {id: 1, content: '2번', done: true},
+  ]
 </script>
 
-<button on:click={handleLogin}>Log In</button>
-<button on:click={handleLogout}>Log Out</button>
-
-{#if auth.loggedIn === true} 
-<p>로그인 중입니다.</p>
-{:else}
-<p>로그아웃 중입니다.</p>
-{/if}
+<ul>
+  {#each todos as {id, content, done}, index}
+    <li>
+      <span>{index + 1}</span>,
+      <span>{content}</span>,
+      <span>{done}</span>
+    </li>
+  {/each}
+</ul>
