a708812e060e5667a2ca0a84e13e411a8ac340ed
Tory <88531407+1two13@users.noreply.github.com>
2023-10-11 17:25:43 +0900

    bind(사용자 정의 컴포넌트)

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,9 @@
 <script>
-  let selected = 0
+  import CountComp from "./countComp.svelte"
+
+  let viewValue = 0
 </script>
 
-<h2>size</h2>
+<h2>{viewValue}</h2>
 
-<label>
-  <input type='radio' bind:group={selected} value={1}>
-  One Scoop
-</label>
-
-<label>
-	<input type=radio bind:group={selected} value={2}>
-	Two scoops
-</label>
-
-<p>선택된 라이도 버튼: {selected} 번째</p>
+<CountComp bind:test={viewValue}/>
---
+++ src/countComp.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let test = 0 // props
+
+  function add() {
+    test += 1;
+  }
+</script>
+
+<button on:click={add}>ADD</button>
