0e14789ae3ae064cafb773c4d32e064e46ecc6fa
Tory <88531407+1two13@users.noreply.github.com>
2023-10-11 16:41:19 +0900

    {#if ...}

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,17 @@
 <script>
-  import PanelComponent from './panelComponent.svelte'
+  let auth = {
+    loggedIn: false
+  }
+
+  const handleLogin = () => auth.loggedIn = true;
+  const handleLogout = () => auth.loggedOut = false;
 </script>
 
-<PanelComponent />
+<button on:click={handleLogin}>Log In</button>
+<button on:click={handleLogout}>Log Out</button>
+
+{#if auth.loggedIn === true} 
+<p>로그인 중입니다.</p>
+{:else}
+<p>로그아웃 중입니다.</p>
+{/if}
