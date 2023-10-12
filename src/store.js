6557ec617543809bf3c64b0626465d560e671719
Tory <88531407+1two13@users.noreply.github.com>
2023-10-12 13:39:45 +0900

    feat: store 생성

---
+++ src/store.js
@@ -0,0 +1,129 @@
+import { writable } from 'svelte/store';
+import { v4 as uuid } from 'uuid';
+import Constant from './constant';
+
+function setFormTodo() {
+  const todoValue = 'todo';
+
+  const { subscribe, update, set } = writable(todoValue);
+
+  const resetForm = () => {
+    update((todoValue) => {
+      todoValue = '';
+      return todoValue;
+    });
+  };
+
+  return {
+    subscribe,
+    set,
+    resetForm,
+  };
+}
+
+function setTodoData() {
+  const todoData = {
+    todoList: [
+      {
+        id: uuid(),
+        content: '첫 번째 할일',
+        done: false,
+      },
+      {
+        id: uuid(),
+        content: '두 번째 할일',
+        done: false,
+      },
+      {
+        id: uuid(),
+        content: '세 번째 할일',
+        done: true,
+      },
+      {
+        id: uuid(),
+        content: '네 번째 할일',
+        done: false,
+      },
+    ],
+    editMode: '',
+    viewMode: Constant.ALL,
+  };
+
+  const { subscribe, update } = writable(todoData);
+
+  const addTodo = (content) => {
+    const newTodo = {
+      id: uuid(),
+      content: content,
+      done: false,
+    };
+
+    update((datas) => {
+      const setData = [...datas.todoLists, newTodo];
+      datas.todoList = setData;
+      return datas;
+    });
+  };
+
+  const editTodo = (editTodo) => {
+    update((datas) => {
+      const setData = datas.todoLists.map((todo) => {
+        if (todo.id === editTodo.id) {
+          todo = editTodo;
+        }
+        return todo;
+      });
+      datas.todoLists = setData;
+      return datas;
+    });
+  };
+
+  const removeTodo = (id) => {
+    update((datas) => {
+      const setData = data.todoLists.filter((todo) => todo.id !== id);
+      datas.todoLists = setData;
+      return datas;
+    });
+  };
+
+  const checkTodo = (id) => {
+    update((datas) => {
+      const setData = data.todoLists.map((todo) => {
+        if (todo.id === id) {
+          todo.done = !todo.done;
+        }
+        return todo;
+      });
+
+      datas.todoLists = setData;
+      return datas;
+    });
+  };
+
+  const changeTodoEditMode = (id) => {
+    update((datas) => {
+      datas.editMode = id;
+      return datas;
+    });
+  };
+
+  const changeTodoView = (mode) => {
+    update((datas) => {
+      datas.viewMode = mode;
+      return datas;
+    });
+  };
+
+  return {
+    subscribe,
+    addTodo,
+    editTodo,
+    removeTodo,
+    checkTodo,
+    changeTodoEditMode,
+    changeTodoView,
+  };
+}
+
+export const todoForm = setTodoData();
+export const Todos = setTodoData();
